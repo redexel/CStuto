4194e621971a1cb6121e51a4fa9a1e58afea620f
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-09-02 03:23:13 +0000

    + 51, 52

---
+++ p51.hs
@@ -0,0 +1,4 @@
+--Not sure how to do this. I'll cheat here: Sloane A000170.
+--Maybe add an actual solution if I figure this one out.
+solutions = [2, 10, 4, 40, 92, 352, 724]
+run = product solutions
