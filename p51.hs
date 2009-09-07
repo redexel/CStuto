50f12c8c00b2dbe4bdf83422d4413b833aa94d90
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-09-07 16:55:06 +0000

    Actual nqueens algo for p51

--- p51.hs
+++ p51.hs
@@ -1,4 +1,6 @@
---Not sure how to do this. I'll cheat here: Sloane A000170.
---Maybe add an actual solution if I figure this one out.
-solutions = [2, 10, 4, 40, 92, 352, 724]
-run = product solutions
+--Changed to actual nqueens algorithm.
+nqueens n = foldl putq [[]] [1..n] where
+	putq pos y = [((x,y):ys) | ys<-pos, x<-[1..n], all (safe (x,y)) ys]
+	safe (x1,y1) (x2,y2) = x1/=x2 && y1/=y2 && abs (x1-x2) /= abs (y1-y2)
+
+run = product $ map (fromIntegral . length . nqueens) [4..10]
