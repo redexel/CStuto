c78760e948de791e9e537755bc6dcac41fe5a909
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-09-09 03:35:45 +0000

    + p57

---
+++ p57.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <algorithm>
+
+int main(){
+	char s[] = "abcdefghijklmno";
+	for(int i=0; i<123456788; i++)
+		//STD is sometimes really great :)
+		std::next_permutation(s,s+15);
+	printf("%s", s);
+	return 0;
+}
