81fed54192b15a8bc7213d5a0f472c1a2cf784b4
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-09-12 22:14:07 +0000

    + p68

---
+++ p68.hs
@@ -0,0 +1,6 @@
+primes = 2:filter isprime [3,5..] where
+	isprime n = all (notdiv n) $ takeWhile (\x->x*x<=n) primes
+	notdiv n x = n `mod` x /= 0
+tree = tree' 0 primes where
+	tree' n xs = take n xs : tree' (n+1) (drop n xs)
+run = sum $ tree !! 100
