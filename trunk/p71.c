c6f038dc8381523a0e3a31794cb1b66a483976f1
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-09-13 04:51:35 +0000

    + p71, p72

---
+++ trunk/p71.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Basically copy & paste from p69.
+//40 seconds with -O3.
+int main(){
+	char* s = malloc(10);
+	int r,j,i,p,o,h,q,z=0,c=0;
+	for(; c<=1000000000; c++){
+		j=0;
+		q=c;
+		while(q>0){
+			r=q%8;
+			q/=8;
+			s[j++]=r+'0';
+		}
+		p=1;
+		o=j%2==0?-1:0;
+		for(i=0, h=j/2; i<=h; i++){
+			if(s[h-i]!=s[h+i+o]){
+				p=0;
+				break;
+			}
+		}
+		if(p) z++;
+	}
+	free(s);
+	printf("%o", z+1);
+	return 0;
+}
