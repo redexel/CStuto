552c5d2a439bd26e97d9135b03a0d7bcca8b5efb
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-09-13 04:51:35 +0000

    + p71, p72

---
+++ p72.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char hex[] = "0123456789abcdef";
+
+//Very similar to p69 and p71.
+//30 seconds with -O3.
+//The really big upper limit renders anything but C impractical
+//for this sort of problem.
+int main(){
+	char* s = malloc(10);
+	int r,j,i,p,o,h,q,z=0,c=0;
+	for(; c<=1000000000; c++){
+		j=0;
+		q=c;
+		while(q>0){
+			r=q%16;
+			q/=16;
+			s[j++]=hex[r];
+		}
+		p=1;
+		o=j%2==0?-1:0;
+		for(i=0, h=j/2; i<=h; i++){
+			if(s[h-i]!=s[h+i+o]){
+				p=0;
+				break;
+			}
+		}
+		if(p) z++;
+	}
+	free(s);
+	printf("%x", z+1);
+	return 0;
+}
