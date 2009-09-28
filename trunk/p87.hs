296458173ef919f26ae1f4f9eb1b5ce3588153f4
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-09-28 23:09:39 +0000

    + p87

---
+++ trunk/p87.hs
@@ -0,0 +1,9 @@
+import Data.List
+
+binary = reverse . binary' where
+	binary' 0 = ""
+	binary' x = let (d,m) = x `divMod` 2 in
+		(show m) ++ binary' d
+
+run = sum . take 1000 . filter (\x -> x `mod` 19 == 0) $
+	map (read . binary) [1..]
