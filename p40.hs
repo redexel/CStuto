3d4b6a1e4504e1cc2fbdb2b8610ff04a7f30ba12
bai.li.2005 <bai.li.2005@90fee466-974e-11de-bf47-b33d4a03a5a8>
2009-10-22 02:10:03 +0000

    slightly improving p40.

--- p40.hs
+++ p40.hs
@@ -1,5 +1,6 @@
-pascal = iterate next [1] where
-	next xs = [head xs] ++ (take (length xs) $ zipWith mplus xs (tail xs)) ++ [last xs]
-	mplus a b = mod (a+b) 100
---Note: runs in about 2 minutes.
-run = sum $ map sum $ take 25000 pascal
+sumpascal n = next (n-1) 1 [] where
+	next 0 m _ = m
+	next r m lst = next (r-1) (m + sum this) this where
+		this = 1 : zipWith (\a b -> (a+b) `mod` 100) lst (tail lst) ++ [1]
+
+run = sumpascal 25000
