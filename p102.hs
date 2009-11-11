7209917662df0d25fba8a15ec0b9aaf20864d87e
bai.li.2005 <bai.li.2005@gmail.com>
2009-11-11 02:56:45 +0000

    + p102

---
+++ p102.hs
@@ -0,0 +1,12 @@
+
+sums xs = map snd . filter possible . init . zip (map (flip drop xs) [1..]) $
+	map (sum . flip take xs) [1..length xs] where
+		possible ([], _) = True
+		possible (ys, sm) = let
+			tl = length . takeWhile (<=sm) $ scanl1 (+) ys
+			in if sum (take tl ys) == sm then possible ((drop tl ys), sm) else False
+
+main = do
+	f <- readFile "p102.txt"
+	print . head . sums . concatMap (map rd . words) . lines $ f where
+		rd n = read n :: Int
