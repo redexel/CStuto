39bfe07d92b31ffadbaa7558ccfef7a796318945
bai.li.2005 <bai.li.2005@gmail.com>
2009-12-14 04:31:33 +0000

    Problem 104 changed

--- p104.hs
+++ p104.hs
@@ -3,4 +3,4 @@ wds = words "no pa re ci vo mu xa ze bi so"
 wds_o = [1,0,0,0,1,0,0,0,0,1]
 os n = sum . map ((wds_o !!) . digitToInt) $ show n
 lojban n = concatMap ((wds !!) . digitToInt) $ show n
-run = lojban . sum $ map os [1..9999]
+run = lojban . sum $ map os [1..10000]
